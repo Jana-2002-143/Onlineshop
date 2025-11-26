be2f67afa3643d324887b8893ef5819396473003
Jana-2002-143 <janaganl1011@gmail.com>
2025-11-26 11:04:57 +0530

    changes

--- src/Components/Signuppage/Signup.jsx
+++ src/Components/Signuppage/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Signup.css";
-import { Await } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
   const [uservalue, setUservalue] = useState("");
@@ -18,8 +18,7 @@ function Signup() {
     setUservalue(e.target.value);
     if (e.target.value === "") {
       setUser(true);
-    }
-    else{
+    } else {
       setUser(false);
     }
   };
@@ -27,8 +26,7 @@ function Signup() {
     setUserpassvalue(e.target.value);
     if (e.target.value === "") {
       setUserpass(true);
-    }
-    else{
+    } else {
       setUserpass(false);
     }
   };
@@ -36,8 +34,7 @@ function Signup() {
     setUserrepassvalue(e.target.value);
     if (e.target.value === "") {
       setUserrepass(true);
-    }
-    else{
+    } else {
       setUserrepass(false);
     }
   };
@@ -45,8 +42,7 @@ function Signup() {
     setUserphonevalue(e.target.value);
     if (e.target.value === "") {
       setUserphone(true);
-    }
-    else{
+    } else {
       setUserphone(false);
     }
   };
@@ -54,8 +50,7 @@ function Signup() {
     setUseremailvalue(e.target.value);
     if (e.target.value === "") {
       setUseremail(true);
-    }
-    else{
+    } else {
       setUseremail(false);
     }
   };
@@ -70,31 +65,35 @@ function Signup() {
     else if (!/^\d+$/.test(userphonevalue)) {
       return "Phone must contain only numbers";
     } else if (userphonevalue.length !== 10) {
-     return "Phone must be exactly 10 digits";
+      return "Phone must be exactly 10 digits";
     }
 
-    const datalist={
-      name:uservalue,
-      password:userpassvalue,
-      email:useremailvalue,
-      phoneno:userphonevalue
-    }
+    const datalist = {
+      name: uservalue,
+      password: userpassvalue,
+      email: useremailvalue,
+      phoneno: userphonevalue,
+    };
 
     try {
-      const response=await fetch("https://onlineshop-backend-vvjx.onrender.com/api/signup",{
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify(datalist),
-      });
+      const response = await fetch(
+        "https://onlineshop-backend-vvjx.onrender.com/api/signup",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(datalist),
+        }
+      );
 
-      if(response.ok){
-        const data=await response.json();
-        localStorage.setItem("username",data.name);
-        localStorage.setItem("useremail",data.email);
-        localStorage.setItem("userphoneno",data.phoneno);
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("username", data.name);
+        localStorage.setItem("useremail", data.email);
+        localStorage.setItem("userphoneno", data.phoneno);
 
         alert("Account Created");
-      }else {
+        useNavigate("/Login");
+      } else {
         const errorMsg = await response.text();
         alert("Signup Failed: " + errorMsg);
       }
@@ -108,23 +107,48 @@ function Signup() {
       <div className="Signupcontainer">
         <form action="onSubmit" onSubmit={signupbtn}>
           <label htmlFor="userinput1">UserName</label>
-          <input type="text" onChange={username} placeholder="User Name" id="userinput1" />
+          <input
+            type="text"
+            onChange={username}
+            placeholder="User Name"
+            id="userinput1"
+          />
           {user && <p className="inputempty">Please Enter Your UserName</p>}
           <label htmlFor="userinput2">Password</label>
-          <input type="password" onChange={userpassword} placeholder="Pasword" id="userinput2" />
+          <input
+            type="password"
+            onChange={userpassword}
+            placeholder="Pasword"
+            id="userinput2"
+          />
           {userpass && <p className="inputempty">Please Enter Your Password</p>}
           <label htmlFor="userinput3">Re Password</label>
-          <input type="password" onChange={userrepassword} placeholder=" Retype Password" id="userinput3" />
+          <input
+            type="password"
+            onChange={userrepassword}
+            placeholder=" Retype Password"
+            id="userinput3"
+          />
           {userrepass && (
             <p className="inputempty">Please Enter Your Password</p>
           )}
           <label htmlFor="userinput4">Phoneno</label>
-          <input type="number" onChange={userphoneno} placeholder="Phone Number" id="userinput4"/>
+          <input
+            type="number"
+            onChange={userphoneno}
+            placeholder="Phone Number"
+            id="userinput4"
+          />
           {userphone && (
             <p className="inputempty">Please Enter your Phone number</p>
           )}
           <label htmlFor="userinput5">Email</label>
-          <input type="Email" onChange={useremailid} placeholder="Email" id="userinput5" />
+          <input
+            type="Email"
+            onChange={useremailid}
+            placeholder="Email"
+            id="userinput5"
+          />
           {useremail && <p className="inputempty">Please Enter Your Email</p>}
           <button>Signup</button>
         </form>
