a8f019143bca174309b533d52efdf31bc5820333
Jana-2002-143 <janaganl1011@gmail.com>
2025-12-06 11:19:33 +0530

    changes

--- src/Reusable/Header/Header.jsx
+++ src/Reusable/Header/Header.jsx
@@ -31,8 +31,9 @@ function Header() {
   const history = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("", {
+      const response = await fetch("https://productbackend-oi15.onrender.com/api/history", {
         method: "GET",
+        headers:{"Content-type":"application/json",}
       });
       const data = await response.json();
       console.log(data);
