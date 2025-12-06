06302f4be2a2b2981810eac62f3437ce5ee95423
Jana-2002-143 <janaganl1011@gmail.com>
2025-12-06 15:36:39 +0530

    changes

--- src/Components/Cartpage/Cart.jsx
+++ src/Components/Cartpage/Cart.jsx
@@ -91,7 +91,7 @@ function Cart() {
     };
     try {
       const response = await fetch(
-        "https://productbackend-oi15.onrender.com/api/order",
+        `https://productbackend-oi15.onrender.com/api/history/${buyerName}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
